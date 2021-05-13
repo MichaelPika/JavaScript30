c2401b47f587f98f3f42fdb3972605405c53e27d
Mikhail Pryka <xauru7@gmail.com>
2021-05-14 01:08:13 +0300

    feat: add keyboard navigation

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -16,3 +16,56 @@ panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
 panels.forEach((panel) =>
 	panel.addEventListener("transitionend", toggleActive)
 );
+
+document.addEventListener("keydown", function (event) {
+	if (event.code == "ArrowLeft" || event.code == "ArrowRight") {
+		let openPanel = document.getElementsByClassName("open");
+
+		if (openPanel.length == 1) {
+			let openPanelArray = document.getElementsByClassName("panel");
+			let openElement = removePanelClass(openPanelArray);
+
+			if (event.code == "ArrowLeft") {
+				openNewPanelByLeft(openElement);
+			} else {
+				openNewPanelByRight(openElement);
+			}
+		} else return;
+	}
+});
+
+let removePanelClass = (openPanelArray) => {
+	let allPanelArray = [];
+	for (let arrayPanel in openPanelArray) {
+		if (openPanelArray[arrayPanel].classList.contains("open")) {
+			openPanelArray[arrayPanel].classList.remove("open");
+			allPanelArray.push(openPanelArray);
+			allPanelArray.push(openPanelArray[arrayPanel]);
+			allPanelArray.push(arrayPanel);
+			return allPanelArray;
+		} else continue;
+	}
+};
+let openNewPanelByLeft = (openElement) => {
+	let openElementArray = openElement[0];
+	let openPanelElement = openElement[1];
+	let openElementIndex = openElement[2];
+
+	if (openPanelElement.classList.contains("panel1")) {
+		openElementArray[4].classList.add("open");
+	} else {
+		openElementArray[openElementIndex - 1].classList.add("open");
+	}
+};
+
+let openNewPanelByRight = (openElement) => {
+	let openElementArray = openElement[0];
+	let openPanelElement = openElement[1];
+	let openElementIndex = openElement[2];
+
+	if (openPanelElement.classList.contains("panel5")) {
+		openElementArray[0].classList.add("open");
+	} else {
+		openElementArray[+openElementIndex + 1].classList.add("open");
+	}
+};
