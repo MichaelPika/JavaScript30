f81f37964ea82716e11d08facf34b4aacf37bebd
Mikhail Pryka <xauru7@gmail.com>
2021-05-30 22:22:56 +0300

    fix: function switch panels

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,71 +1,51 @@
 const panels = document.querySelectorAll(".panel");
+let nextPanelIndex = 0;
 
 function toggleOpen() {
-	console.log("Hello");
 	this.classList.toggle("open");
 }
 
 function toggleActive(e) {
-	console.log(e.propertyName);
 	if (e.propertyName.includes("flex")) {
 		this.classList.toggle("open-active");
 	}
 }
 
+let switchPanel = (event) => {
+	if (event.code == "ArrowLeft" || event.code == "ArrowRight") {
+		let openPanels = document.getElementsByClassName("open");
+
+		if (openPanels.length == 1) {
+			let indexElement = removePanelClass(panels);
+
+			if (event.code == "ArrowLeft") {
+				openNewPanelByLeft(indexElement);
+			} else {
+				openNewPanelByRight(indexElement);
+			}
+		}
+	}
+};
+
+document.addEventListener("keydown", switchPanel);
 panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
 panels.forEach((panel) =>
 	panel.addEventListener("transitionend", toggleActive)
 );
 
-document.addEventListener("keydown", function (event) {
-	if (event.code == "ArrowLeft" || event.code == "ArrowRight") {
-		let openPanel = document.getElementsByClassName("open");
-
-		if (openPanel.length == 1) {
-			let openPanelArray = document.getElementsByClassName("panel");
-			let openElement = removePanelClass(openPanelArray);
-
-			if (event.code == "ArrowLeft") {
-				openNewPanelByLeft(openElement);
-			} else {
-				openNewPanelByRight(openElement);
-			}
-		} else return;
-	}
-});
-
-let removePanelClass = (openPanelArray) => {
-	let allPanelArray = [];
-	for (let arrayPanel in openPanelArray) {
-		if (openPanelArray[arrayPanel].classList.contains("open")) {
-			openPanelArray[arrayPanel].classList.remove("open");
-			allPanelArray.push(openPanelArray);
-			allPanelArray.push(openPanelArray[arrayPanel]);
-			allPanelArray.push(arrayPanel);
-			return allPanelArray;
-		} else continue;
+let removePanelClass = (panels) => {
+	for (let elementOfPanels in panels) {
+		if (panels[elementOfPanels].classList.contains("open")) {
+			panels[elementOfPanels].classList.remove("open");
+			return elementOfPanels;
+		}
 	}
 };
-let openNewPanelByLeft = (openElement) => {
-	let openElementArray = openElement[0];
-	let openPanelElement = openElement[1];
-	let openElementIndex = openElement[2];
-
-	if (openPanelElement.classList.contains("panel1")) {
-		openElementArray[4].classList.add("open");
-	} else {
-		openElementArray[openElementIndex - 1].classList.add("open");
-	}
+let openNewPanelByLeft = (indexElement) => {
+	nextPanelIndex = indexElement - 1 < 0 ? panels.length - 1 : indexElement - 1;
+	panels[nextPanelIndex].classList.add("open");
 };
-
-let openNewPanelByRight = (openElement) => {
-	let openElementArray = openElement[0];
-	let openPanelElement = openElement[1];
-	let openElementIndex = openElement[2];
-
-	if (openPanelElement.classList.contains("panel5")) {
-		openElementArray[0].classList.add("open");
-	} else {
-		openElementArray[+openElementIndex + 1].classList.add("open");
-	}
+let openNewPanelByRight = (indexElement) => {
+	nextPanelIndex = +indexElement + 1 < panels.length ? +indexElement + 1 : 0;
+	panels[nextPanelIndex].classList.add("open");
 };
